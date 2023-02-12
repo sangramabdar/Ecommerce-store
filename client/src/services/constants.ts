dd35fa4bc1ccebeb937b3a77eb932da7f3e39b17
sangramabdar <abdarsangram2697@gmail.com>
2023-02-12 12:57:02 +0530

    latest build

--- client/src/services/constants.ts
+++ client/src/services/constants.ts
@@ -16,9 +16,9 @@ const DEFAULT_HEADERS = {
   "Content-Type": "application/json",
 };
 
-// const BASE_URL = "https://ecommerce-store-9an7.vercel.app/api";
+const BASE_URL = "https://ecommerce-store-9an7.vercel.app/api";
 
-const BASE_URL = "http://localhost:8080/api";
+// const BASE_URL = "http://localhost:8080/api";
 
 export { RequestStatus, BASE_URL, DEFAULT_HEADERS };
 export type { Result };
