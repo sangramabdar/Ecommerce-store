ecad4d0125b00aebf8f063dcee0ed583d8676c18
sangramabdar <abdarsangram2697@gmail.com>
2024-01-22 13:47:37 +0530

    updated vercel.json

--- server/build/index.js
+++ server/build/index.js
@@ -1,5 +1,16 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv").config();
-const initServer_1 = require("./config/initServer");
-(0, initServer_1.initServer)();
+const express = require("express");
+
+const app = express();
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is my about route..... ");
+});
+
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `);
+});
