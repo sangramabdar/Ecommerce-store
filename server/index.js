3d52fd01bf2038be16d6ce38a22a090cdf16e79d
sangramabdar <abdarsangram2697@gmail.com>
2024-01-22 14:46:34 +0530

    trying to switch to render

--- server/index.js
+++ server/index.js
@@ -1,5 +1,15 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv").config();
-const initServer_1 = require("./build/config/initServer");
-(0, initServer_1.initServer)();
+// "use strict";
+// Object.defineProperty(exports, "__esModule", { value: true });
+// require("dotenv").config();
+// const initServer_1 = require("./build/config/initServer");
+// (0, initServer_1.initServer)();
+
+const app = require("express");
+
+app.use("*", (req, res) => {
+  res.send("server is ready");
+});
+
+app.listen(8080, () => {
+  console.log("server started on port : ", 8080);
+});
