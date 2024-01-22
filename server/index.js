10e0df613695601e22b24714478b134c76929925
sangramabdar <abdarsangram2697@gmail.com>
2024-01-22 14:04:10 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -1,20 +1,3 @@
-// const express = require("express");
-
-// const app = express();
-// const PORT = 4000;
-
-// app.get("/", (req, res) => {
-//   res.send("Hey this is my API running 🥳");
-// });
-
-// app.get("/about", (req, res) => {
-//   res.send("This is my about route..... ");
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`API listening on PORT ${PORT} `);
-// });
-
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv").config();
