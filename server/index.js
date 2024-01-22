37d95c28f46583e47b221c8a32f4bb1f10332a84
sangramabdar <abdarsangram2697@gmail.com>
2024-01-22 13:02:23 +0530

    updated vercel.json

--- server/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv").config();
-const initServer_1 = require("./build/config/initServer");
-(0, initServer_1.initServer)();
