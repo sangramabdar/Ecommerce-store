4aee65475fbc61a687cc2176bfb92fd67daa5ab8
sangramabdar <abdarsangram2697@gmail.com>
2024-01-22 15:37:46 +0530

    latest build

--- server/index.js
+++ server/index.js
@@ -1,17 +1,5 @@
-// "use strict";
-// Object.defineProperty(exports, "__esModule", { value: true });
-// require("dotenv").config();
-// const initServer_1 = require("./build/config/initServer");
-// (0, initServer_1.initServer)();
-
-// const express = require("express");
-
-// const app = express();
-
-// app.use("*", (req, res) => {
-//   res.send("server is ready");
-// });
-
-// app.listen(8080, () => {
-//   console.log("server started on port : ", 8080);
-// });
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv").config();
+const initServer_1 = require("./build/config/initServer");
+(0, initServer_1.initServer)();
