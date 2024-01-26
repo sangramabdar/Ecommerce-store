0dc636a2c6bec0d6a9e99df7fc2dfad781f61d8d
sangramabdar <abdarsangram2697@gmail.com>
2024-01-26 10:38:17 +0530

    refactored code at server

--- server/src/utils/responseBodyBuilder.ts
+++ server/src/utils/responseBodyBuilder.ts
@@ -1,12 +1,12 @@
 class ResponseBodyBuilder<T = any> {
   private timeStamp: number = Date.now();
-  private error: string = "";
+  private errors: any[] = [];
   private statusCode: number = 200;
   private data: T | {} = null;
   private ok: boolean = true;
 
-  setError(error: string) {
-    this.error = error;
+  setErrors(errors: any[]) {
+    this.errors = errors;
     return this;
   }
 
@@ -17,6 +17,11 @@ class ResponseBodyBuilder<T = any> {
 
   setData(data: T) {
     this.data = data;
+    return this;
+  }
+
+  setOK(ok: boolean) {
+    this.ok = ok;
     return this;
   }
 
