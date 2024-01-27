22dbd957dd697133a4611781d5baa50194859134
sangramabdar <abdarsangram2697@gmail.com>
2024-01-27 16:04:32 +0530

    latest build

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import app from "./config/express.config";
 
 console.log(environmentConfig);
 
+//hello
 app.listen(environmentConfig.PORT, () => {
   console.log("Server is started on port :", environmentConfig.PORT);
 });
