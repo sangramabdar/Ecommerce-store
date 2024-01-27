a0c77f022deea84880470efc416f684f3c716745
sangramabdar <abdarsangram2697@gmail.com>
2024-01-27 15:33:10 +0530

    fix:latest build

--- server/index.js
+++ server/index.js
@@ -1,5 +1,17 @@
 "use strict";
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv").config();
-const initServer_1 = require("./build/config/initServer");
-(0, initServer_1.initServer)();
+const environment_config_1 = __importDefault(
+  require("./build/config/environment.config")
+);
+const express_config_1 = __importDefault(
+  require("./build/config/express.config")
+);
+console.log(environment_config_1.default);
+express_config_1.default.listen(environment_config_1.default.PORT, () => {
+  console.log("Server is started on port :", environment_config_1.default.PORT);
+});
