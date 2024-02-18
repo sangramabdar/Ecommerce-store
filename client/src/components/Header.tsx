0a12ea29c6ccb9677cbd11e6a53616e473d9a3c8
sangramabdar <abdarsangram2697@gmail.com>
2024-02-18 11:04:01 +0530

    added accountt drop down in client

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,93 +1,18 @@
-import { Link, useNavigate } from "react-router-dom";
-import { MouseEventHandler } from "react";
-import { motion } from "framer-motion";
-import cn from "../utils/cn";
+import { Link } from "react-router-dom";
+
 import { useQuery } from "@tanstack/react-query";
 import { getCartItemsService } from "../features/cart/cart.service";
 
 import { FaShoppingCart } from "react-icons/fa";
 import { useAuthContext } from "./auth";
-
-function SideNavigation({ open, onClick }: { open: boolean; onClick: any }) {
-  return (
-    <div
-      className={cn(
-        "block md:hidden absolute right-0 top-0 h-screen transition-all duration-200 bg-gray-100/80 backdrop-blur-lg w-full",
-        open ? "translate-x-0" : "translate-x-[100%]"
-      )}
-    >
-      <div className="flex flex-col justify-between items-start gap-6 pt-12 pl-4 font-bold text-xl">
-        <Link
-          className="bg-accent p-2 px-4 rounded-md text-white"
-          to="/login"
-          onClick={onClick}
-        >
-          Login
-        </Link>
-        <Link
-          className="bg-accent p-2 px-4 rounded-md text-white"
-          to="/signup"
-          onClick={onClick}
-        >
-          Sign Up
-        </Link>
-      </div>
-    </div>
-  );
-}
-
-function LogOutSideNavigation({
-  open,
-  cartItems,
-  handleLogOut,
-  onClick,
-}: {
-  open: boolean;
-  cartItems: any[];
-  handleLogOut: MouseEventHandler<HTMLButtonElement>;
-  onClick: any;
-}) {
-  return (
-    <motion.div
-      className={cn(
-        "block mds:hidden absolute right-0 top-0 h-screen transition-all duration-200 bg-gray-100/80 backdrop-blur-lg w-full",
-        open ? "translate-x-0" : "translate-x-[100%]"
-      )}
-    >
-      <div className="flex flex-col justify-between items-start gap-6 pt-12 pl-4 font-bold text-xl">
-        <Link className="flex" to="/cart" onClick={onClick}>
-          <span>Cart</span>
-          {cartItems.length > 0 && (
-            <p className="font-bold">: {cartItems.length}</p>
-          )}
-        </Link>
-
-        <button
-          className="bg-accent p-2 px-4 rounded-md text-white"
-          onClick={handleLogOut}
-        >
-          Log out
-        </button>
-      </div>
-    </motion.div>
-  );
-}
+import AccountDropDown from "../features/account/components/account-drop-down";
 
 function Header() {
-  const { user, removeUser } = useAuthContext();
-
-  const navigate = useNavigate();
-
-  const handleLogOut = () => {
-    localStorage.removeItem("user");
-    removeUser();
-    navigate("/");
-    location.reload();
-  };
+  const { user } = useAuthContext();
 
   if (!user) {
     return (
-      <header className="bg-secondary top-0 left-0 right-0 fixed border-b-2">
+      <header className="bg-secondary top-0 left-0 right-0 fixed border-b-2 z-10 ">
         <nav className="flex max-w-7xl mx-auto p-4 px-4 sm:px-8 justify-between items-center">
           <Link to="/" className="font-bold text-base md:text-xl">
             E-COMMERCE STORE
@@ -113,7 +38,7 @@ function Header() {
   }
 
   return (
-    <header className="bg-secondary top-0 left-0 right-0 fixed border-b-2">
+    <header className="bg-secondary top-0 left-0 right-0 fixed border-b-2  z-10">
       <nav className="flex max-w-7xl mx-auto px-4 sm:px-8 justify-between items-center p-4">
         <Link to="/" className="font-bold text-base md:text-xl">
           E-COMMERCE STORE
@@ -121,12 +46,7 @@ function Header() {
 
         <div className="flex justify-evenly gap-3 items-center">
           <CartIcon />
-          <button
-            className="bg-accent py-1 px-2 rounded-md text-white"
-            onClick={handleLogOut}
-          >
-            Log out
-          </button>
+          <AccountDropDown />
         </div>
       </nav>
     </header>
@@ -151,7 +71,7 @@ function CartIcon() {
     return (
       <Link to={"/cart"}>
         <div className="relative">
-          <FaShoppingCart className="w-8 h-8" />
+          <FaShoppingCart className="w-6 h-6" />
         </div>
       </Link>
     );
@@ -160,8 +80,8 @@ function CartIcon() {
   return (
     <Link to={"/cart"}>
       <div className="relative">
-        <FaShoppingCart className="w-8 h-8" />
-        <p className="absolute top-[-14px] flex justify-center items-center bg-secondary w-6 h-6 p-1 rounded-full right-[-10px]">
+        <FaShoppingCart className="w-6 h-6" />
+        <p className="absolute top-[-10px] flex justify-center items-center bg-secondary w-4 h-4 p-1 rounded-full right-[-10px]">
           {isLoading || data?.cartItems?.length}
         </p>
       </div>
---
+++ client/src/features/account/components/account-drop-down.tsx
@@ -0,0 +1,115 @@
+import { MdAccountCircle } from "react-icons/md";
+
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import cn from "../../../utils/cn";
+import Button from "../../../components/ui/button";
+import { useAuthContext } from "../../../components/auth";
+
+function AccountDropDown() {
+  const { removeUser } = useAuthContext();
+
+  const navigate = useNavigate();
+
+  const handleLogOut = () => {
+    localStorage.removeItem("user");
+    removeUser();
+    navigate("/");
+    location.reload();
+  };
+
+  return (
+    <Menu as="div" className="relative flex">
+      <Menu.Button className="focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 ">
+        <MdAccountCircle className="w-8 h-8" />
+      </Menu.Button>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 top-10 mt-2 w-52 divide-y divide-gray-00 rounded-md bg-secondary shadow-md ring-1 ring-black/5 focus:outline-none">
+          <div className="px-1 py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <Link
+                  to={"/acoount/profile"}
+                  className={cn(
+                    "group flex w-full items-center rounded-md px-2 py-2 text-sm font-semibold",
+                    active ? "bg-accent text-white" : "text-gray-900"
+                  )}
+                >
+                  {/* {active ? (
+                    <div className="mr-2 h-5 w-5" aria-hidden="true" />
+                  ) : (
+                    <div className="mr-2 h-5 w-5" aria-hidden="true" />
+                  )} */}
+                  Profile
+                </Link>
+              )}
+            </Menu.Item>
+          </div>
+          <div className="px-1 py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <Link
+                  to={"/account/orders"}
+                  className={cn(
+                    "group flex w-full items-center rounded-md px-2 py-2 text-sm font-semibold",
+                    active ? "bg-accent text-white" : "text-gray-900"
+                  )}
+                >
+                  {/* {active ? (
+                    <div
+                      className="mr-2 h-5 w-5 text-violet-400"
+                      aria-hidden="true"
+                    />
+                  ) : (
+                    <div
+                      className="mr-2 h-5 w-5 text-violet-400"
+                      aria-hidden="true"
+                    />
+                  )} */}
+                  My Orders
+                </Link>
+              )}
+            </Menu.Item>
+          </div>
+          <div className="px-1 py-2">
+            <Menu.Item>
+              {({ active }) => (
+                <Button
+                  onClick={handleLogOut}
+                  className={cn(
+                    "group flex w-fit items-center justify-center rounded-md px-4 py-2 text-sm font-semibold",
+                    active && "bg-accent text-white"
+                  )}
+                >
+                  {/* {active ? (
+                    <div
+                      className="mr-2 h-5 w-5 text-violet-400"
+                      aria-hidden="true"
+                    />
+                  ) : (
+                    <div
+                      className="mr-2 h-5 w-5 text-violet-400"
+                      aria-hidden="true"
+                    />
+                  )} */}
+                  Log out
+                </Button>
+              )}
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  );
+}
+
+export default AccountDropDown;
